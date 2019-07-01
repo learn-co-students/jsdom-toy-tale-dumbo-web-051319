9f2476ae0f65df1ba32c89248896cc7b37d5611c
DDuongNguyen <duong.nguyen4991@gmail.com>
2019-07-01 15:21:09 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,49 +1,138 @@
-const addBtn = document.querySelector('#new-toy-btn')
-const toyForm = document.querySelector('.container')
-let addToy = false
+const addBtn = document.querySelector('#new-cat-btn')
+const catForm = document.querySelector('.container')
+const catCollection = document.querySelector('#cat-collection')
+const nameInput = catForm.querySelector('#js-input-name')
+const imgInput = catForm.querySelector('#js-input-url')
+let addCat = false
 
 // YOUR CODE HERE
 
 addBtn.addEventListener('click', () => {
   // hide & seek with the form
-  addToy = !addToy
-  if (addToy) {
-    toyForm.style.display = 'block'
+  addCat = !addCat
+  if (addCat) {
+    catForm.style.display = 'block'
     // submit listener here
   } else {
-    toyForm.style.display = 'none'
+    catForm.style.display = 'none'
   }
 })
-//when the page loads, show the toys:
-function slapToysOnDOM(toys){
-  const toyCollection = document.querySelector("#toy-collection")
-  toys.forEach(function(toy){
-    //create divs for each toy object
-    const div = document.createElement("div")
-    div.className = "card"
-    console.log(div)
-    div.innerHTML=`<h2>${toy.name}</h2> <img src="${toy.image}" class="toy-avatar"> <p>"${toy.likes} Likes"</p> <button class="like-btn">Like <3</button>`
-    toyCollection.append(div)
-  })
+//**********************Create Cat********************************
+catForm.addEventListener('submit', (event) => {
+  event.preventDefault()
+  if(event.target.className === "add-cat-form"){
+    // debugger
+    fetch(`http://localhost:3000/toys/`,
+      {method: 'POST',
+        headers: {'content-type': 'application/json'},
+        body: JSON.stringify(
+          {name: `${nameInput.value}`,
+          image: `${imgInput.value}`,
+          likes: `0`
+          })
+      })
+      .then(resp => resp.json())
+      .then(newCat => {
+        catCollection.innerHTML +=
+        `
+    <div class="card" data-catid=${newCat.id}>
+    <h2>${newCat.name}</h2>
+    <img src="${newCat.image}" class="cat-avatar"/>
+    <p >${newCat.likes} like</p>
+    <button class="like-button" data-likes= "${newCat.likes}"> Likes <3 </button>
+    </div>
+    <button class="delete-button">Delete This</button>
+    `
+      })
+  }
+
 }
+)
 
-
-// Each card should have the following child elements:
-//
-//   * `h2` tag with the toy's name
-//   * `img` tag with the `src` of the toy's image attribute and the class name "toy-avatar"
-//   * `p` tag with how many likes that toy has
-//   * `button` tag with an class "like-btn"
-
-
-
-// OR HERE!
-
-document.addEventListener("DOMContentLoaded", function(){
-  const promise = fetch("http://localhost:3000/toys")
-  // console.log(promise)
-  promise.then(response => response.json())
-          .then(toyObject => slapToysOnDOM(toyObject))
+// cat-collection div-name
+// **************** Cat Index from cat API **********************
+document.addEventListener("DOMContentLoaded", function() {
+  // debugger
+  fetch("http://localhost:3000/toys")
+    .then(response => response.json())
+    .then(cats => {
+      cats.forEach(cat => {
+        // debugger
+        const catName = cat.name
+        const catImg = cat.image
+        const catLikes = cat.likes
+        // debugger
+        catCollection.innerHTML +=
+          `
+      <div class="card" data-catid=${cat.id}>
+      <h2>${catName}</h2>
+      <img src="${catImg}" class="cat-avatar"/>
+      <p >${catLikes} like</p>
+      <button class="like-button" data-likes= "${catLikes}"> Likes <3 </button>
+      <button class="delete-button"> Delete This </button>
+      </div>
+      `
+      })
+    })
 })
 
-// toy-collection div-name
+// ******************** cat likes ***********************************
+catCollection.addEventListener("click", function() {
+  if (event.target.className === "like-button") {
+    const catId = event.target.parentElement.dataset.catid
+    // debugger
+    let likes = parseInt(event.target.dataset.likes)
+    let afterLiked = likes + 1
+
+    const fetchCat = (event, catId, afterLiked) => {
+      fetch(`http://localhost:3000/toys/${catId}`, {
+          method: "PATCH",
+          headers: {
+            "content-type": "application/json"
+          },
+          body: JSON.stringify({
+            "likes": afterLiked
+          })
+        })
+        .then(response => response.json())
+        .then(littleMeow => {
+          currentCard = event.target.parentElement
+          currentCard.innerHTML = `
+    <h2>${littleMeow.name}</h2>
+    <img src="${littleMeow.image}" class="cat-avatar"/>
+    <p >${afterLiked} like</p>
+    <button class="like-button" data-likes= "${afterLiked}"> Likes <3 </button>
+    `
+        })
+    }
+    fetchCat(event, catId, afterLiked)
+  }
+})
+// ********************************Delete Cat*******************************
+catCollection.addEventListener('click', function(){
+  if (event.target.className === 'delete-button'){
+    const catId = event.target.parentElement.dataset.catid
+    fetch(`http://localhost:3000/toys/${catId}`, {
+      method: "DELETE"
+    })
+    // debugger
+      event.target.parentElement.remove()
+
+  }
+})
+
+
+
+
+// ***************
+// likes = littleMeow.likes
+// debugger
+//   catCollection.innerHTML =
+//     `
+// <div class="card" data-catid=${cat.id}>
+// <h2>${catName}</h2>
+// <img src="${catImg}" class="cat-avatar"/>
+// <p >${catLikes} like</p>
+// <button class="like-button" data-likes= "${catLikes}"> Likes <3 </button>
+// </div>
+// `
